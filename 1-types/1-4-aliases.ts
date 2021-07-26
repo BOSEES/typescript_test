c2534a5e68ae89e7b553886431939da5fc124e51
bosees <rladuddlf3@naver.com>
2021-07-26 16:55:10 +0900

    TypeScript: Type Alias

---
+++ 1-types/1-4-aliases.ts
@@ -0,0 +1,28 @@
+{
+  /**
+   * Type aliases
+   */
+
+  type Text = string;
+  const name: Text = "duddlf";
+  const address: Text = "korea";
+  type Num = number;
+  type Student = {
+    name: string,
+    age: number,
+  }
+
+  const student: Student = {
+    name: "duddlf",
+    age: 123,
+  }
+
+  //String Literal Types 
+
+  type Name = "name";
+  let ellieName: Name;
+  ellieName = "name";
+  type JSON = "json";
+  let json: JSON = "json";
+
+}
