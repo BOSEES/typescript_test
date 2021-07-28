7f41f2c4cb6dc6c6c8564476fe325cab4e48de15
c <rladuddlf3@naver.com>
2021-07-28 15:10:40 +0900

    TypeScript: 링크스 리스트 구현

---
+++ 4-opp-project/stack.ts
@@ -0,0 +1,60 @@
+{
+  interface Stack {
+    readonly size: number;
+    push(value: string): void;
+    pop():string;
+  }
+  
+  type StackNode = {
+    readonly value: string;
+    readonly next: StackNode | undefined;
+  }
+
+  class LinkedList implements Stack {
+    private _size: number = 0;
+    private head: StackNode | undefined;
+
+    constructor(private capacity: number) {};
+
+    get size() {
+      return this._size;
+    }
+
+    push(value: string): void {
+      if(this.capacity === this.size) {
+        throw new Error("stack is full!");
+      }
+      const node: StackNode = {value: value, next: this.head}
+      this.head = node;
+      this._size++;
+    }
+
+    pop(): string { //null == undefined
+      if(this.head == null) {
+        throw new Error("stack is empty!");
+      }
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
+    }
+  }
+  
+  const stack = new LinkedList(10);
+  stack.push("duddlf1")
+  stack.push("duddlf2")
+  stack.push("duddlf3");
+  stack.push("duddlf1")
+  stack.push("duddlf2")
+  stack.push("duddlf3");
+  stack.push("duddlf1")
+  stack.push("duddlf2")
+  stack.push("duddlf3");
+  stack.push("duddlf1")
+  stack.push("duddlf2")
+  stack.push("duddlf3");
+
+  while(stack.size !== 0) {
+    console.log(stack.pop());
+  }
+}
