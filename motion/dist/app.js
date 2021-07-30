920087fe0b6e4847540a4f9fda4e1364f4c1e717
bosees <rladuddlf3@naver.com>
2021-07-30 10:28:15 +0900

    image components 리팩토링

--- motion/dist/app.js
+++ motion/dist/app.js
@@ -1,11 +1,11 @@
-import { ImageComponents } from "./components/image.js";
-import { PageComponents } from "./components/page.js";
+import { ImageComponents } from "./components/item/image.js";
+import { PageComponents } from "./components/page/page.js";
 class App {
     constructor(appRoot) {
         this.page = new PageComponents();
         this.page.attachTo(appRoot);
-        this.image = new ImageComponents();
-        this.image.attachTo(appRoot);
+        const image = new ImageComponents("image title", "https://picsum.photos/600/300");
+        image.attachTo(appRoot, "beforeend");
     }
 }
 new App(document.querySelector(".document"));
--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,14 +1,14 @@
-import { ImageComponents } from "./components/image.js";
-import { PageComponents } from "./components/page.js";
+import { ImageComponents } from "./components/item/image.js";
+import { PageComponents } from "./components/page/page.js";
 
 class App {
   private readonly page: PageComponents;
-  private readonly image: ImageComponents;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponents();
     this.page.attachTo(appRoot);
-    this.image = new ImageComponents();
-    this.image.attachTo(appRoot);
+    
+    const image = new ImageComponents("image title", "https://picsum.photos/600/300");
+    image.attachTo(appRoot, "beforeend");
   }
 }
 
